19b096ee62a4d7d8bc8940a08c862bbc566a43a6
Stephen Moody <darilligames@gmail.com>
2022-04-30 13:22:55 +0000

    Proto - Basic test files

---
+++ game.js
@@ -0,0 +1,11 @@
+
+let food = ["egg", "bread", "milk", "rice", "pasta", "vegetables", "meat", "snake"];
+
+let animals = ["dog", "cat", "bird", "fish", "cow", "horse", "bee", "fly", "spider"]
+
+
+
+
+let nouns = [];
+
+console.log(animals);
